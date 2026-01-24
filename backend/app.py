60b86f0c8cbd31442b3e337cf9f1d656ed618ae4
Thunder-M1H5S8 <chandaluri.mahi2008@gmail.com>
2026-01-25 01:33:46 +0530

    Allowed origin from frontend to backend

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Hackathon Backend - MVP")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all for hackathon
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
